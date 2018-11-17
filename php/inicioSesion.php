9931e0c16aec4eb33788405952c8ed0a860d26c4
Y. Vasquez <yonaikeravp@gmail.com>
2018-11-17 12:44:57 +0000

    inicioSesion.php ingresa sin mostrar mensajes de alerta

--- php/inicioSesion.php
+++ php/inicioSesion.php
@@ -14,14 +14,14 @@
         
         $sesion = mysqli_query($conexion,"SELECT * FROM usuario WHERE correo='$correo' AND contrasena='$contraseña'");
         
-        echo "VALIDO AL BASE DE DATOS";
+       // echo "VALIDO AL BASE DE DATOS";
         
 
         if( mysqli_num_rows($sesion) > 0 ){
             $fila = mysqli_fetch_array($sesion);
             $_SESSION["correo"] = $fila['correo'];
-            echo 'INICIO DE SESION SATISFACTORIO , AGREGAR LA REDIRECCION';
-            echo "<script> alert('DATOS CORRECTOS')</script>";
+           // echo 'INICIO DE SESION SATISFACTORIO , AGREGAR LA REDIRECCION';
+            //echo "<script> alert('DATOS CORRECTOS')</script>";
             echo '<script> window.location="../vistas/principal.php"; </script>';
         }else{
             echo "<script> alert('USUARIO O CONTRASEÑA INCORRECTOS')</script>";
