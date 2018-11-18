101883d5aa606fac969ed284b81a8026b9c1eaeb
Mary <mariagabriela236@gmail.com>
2018-11-18 01:14:39 +0000

    Deleted bd/.gitkeep, bd/flashmail.sql, php/servidor.php files

--- php/servidor.php
+++
@@ -1,8 +0,0 @@
-<?php 
-        $servidor = "localhost"; //Aqui va el servidor que utilizaremos
-        $usuario = "root";  // aqui va nuestro usuario de la base de datos
-        $contraseña = "";   // contrasela del usuario en caso de tenerla
-        $BD = "flashmail";  // nombre de la base de datos
-
-        $conexion = mysqli_connect($servidor,$usuario,$contraseña,$BD);
-?>
